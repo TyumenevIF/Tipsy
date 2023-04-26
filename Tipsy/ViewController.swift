89a927bc86fce89b6110d717ac9c6e0aa359d6a9
Ilyas Tyumenev <tumenoff@mail.ru>
2023-04-26 15:38:45 +0300

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Ilyas Tyumenev on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
