bde2960e13e2f47e5e272226dbc2f7f4cc5993c4
Ilyas Tyumenev <tumenoff@mail.ru>
2023-04-26 16:18:42 +0300

    Create IBActions and IBOutlets for the Calculator Screen

---
+++ Tipsy/CalculatorViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Ilyas Tyumenev on 25.04.2023.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
